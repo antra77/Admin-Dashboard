cead4199433ecf451ab94bde77d49f1a266be79e
Antra Saraf <antra.saraf@gmail.com>
2024-02-27 17:01:41 +0530

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { SiShopware } from "react-icons/si";
+import { SiZcash } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
@@ -32,7 +32,7 @@ const Sidebar = () => {
               onClick={(handleCloseSidebar) => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Zen</span>
+              <SiZcash /> <span>Zen</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
